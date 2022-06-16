11457d59c88710d9e512bfc77d7ba6bc3525efbb
leojomar01 <leojomar01@gmail.com>
2022-06-16 18:26:29 +0800

    add effect

--- js/animation.js
+++ js/animation.js
@@ -1,9 +1,23 @@
 // create animation div
 
-let shape = 50;
+let shape = 20;
 
-for(var i=0;i<shape;i++){
+for(var i=0;i<=shape;i++){
     let div = document.createElement('div');
     div.className="shape"+i;
     document.getElementById("BGanimation").appendChild(div);
+}
+for(var j=0;j<=shape;j++){
+    let div = document.createElement('div');
+    div.className="shape0"+j;
+    document.getElementById("BGanimation").appendChild(div);
+}
+
+
+shape=30;
+
+for(var x=0;x<=shape;x++){
+    let div2 = document.createElement('div');
+    div2.className="shape"+x;
+    document.getElementById("BGanimation2").appendChild(div2);
 }
